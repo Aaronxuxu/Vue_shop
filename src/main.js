33cf8f0f4b84b21bffd4a025f8bac01465c1c5af
Aaron <470164479@qq.com>
2021-03-18 15:55:24 +0800

    newUpdate

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 // 引入elementui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import 'element-ui/lib/theme-chalk/display.css'
 Vue.use(ElementUI)
+
+// 引入axios
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
---
+++ src/views/login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <header>登录</header>
+    <div class="loginbar">
+      <section>
+        <span id="login-msg" class="hidden-xs-only">账号：</span>
+        <el-input style="" placeholder="请输入账号" v-model="username" clearable  autofocus></el-input>
+      </section>
+      <section>
+        <span id="login-msg" class="hidden-xs-only">密码：</span>
+        <el-input placeholder="请输入密码" v-model="password" show-password clearable ></el-input>
+      </section>
+      <section>
+      <el-button round @click="loginCheck">登录</el-button>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginCheck: async function () {
+      if (this.username === '' || this.password === '') {
+        alert('请输入账号或密码')
+      };
+      this.$axios.post('http://localhost:8888/login', {
+        username: this.username,
+        password: this.password
+      }).then(data => console.log(data))
+      console.log(this.username, this.password)
+    }
+  },
+  beforeCreate () {
+    document.querySelector('body').classList.add('flex')
+  },
+  beforeDestroy () {
+    document.querySelector('body').classList.remove('flex')
+  }
+}
+</script>
+<style scoped>
+.main{
+  color: white;
+}
+header{
+  font-weight:400;
+  font-size: 1.875rem;
+}
+section{
+  margin-top: 1.25rem;
+}
+#login-msg{
+  font-size: 0.9rem;
+}
+.el-input{
+  width: 18.75rem!important;
+}
+</style>
