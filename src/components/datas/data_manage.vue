4fd8b9fb1b43e1a340d7d88647b68a1973e27f28
Aaron <470164479@qq.com>
2021-04-01 18:38:32 +0800

    完成数据报表功能，运用了echarts

--- src/components/datas/data_manage.vue
+++ src/components/datas/data_manage.vue
@@ -1,9 +1,56 @@
 <template>
   <div class="">
-    <h5>数据管理页面</h5>
+    <div id="echart" style="width:600px;height:400px"></div>
   </div>
 </template>
 <script>
-export default {}
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {},
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('echart'))
+    const { data: res } = await this.$axios.get('/reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.showMessage('error', res.meta.msg)
+    }
+    var result = _.merge(res.data, this.options)
+    myChart.setOption(result)
+  }
+}
 </script>
 <style></style>
